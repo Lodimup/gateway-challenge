7c0e2af1ee604d1a309fdf0cc47a5392df0c587c
Pumidol Leelerdsakulvong <pumidol.l@gmail.com>
2024-05-30 17:57:58 +0000

    chore: house keeping

--- app/routers/auths.py
+++ app/routers/auths.py
@@ -16,10 +16,16 @@ router = APIRouter(prefix="/auth", tags=["auth"])
 async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
     user_dict = FAKE_USERS_DB.get(form_data.username)
     if not user_dict:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
+        raise HTTPException(
+            status_code=400,
+            detail="Incorrect username or password",
+        )
     user = UserInDB(**user_dict)
     hashed_password = fake_hash_password(form_data.password)
     if not hashed_password == user.hashed_password:
-        raise HTTPException(status_code=400, detail="Incorrect username or password")
+        raise HTTPException(
+            status_code=400,
+            detail="Incorrect username or password",
+        )
 
     return {"access_token": user.username, "token_type": "bearer"}
